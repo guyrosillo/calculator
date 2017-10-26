d76ac11f9e929cdfdb30bac816a4820e176dd850
guyrosillo <guyrosillo@gmail.com>
2017-10-25 17:03:44 -0700

    First upload

--- js/script.js
+++ js/script.js
@@ -2,6 +2,12 @@
 const buttons = document.querySelectorAll('.button')
 const display = document.querySelector('.display')
 
+clearButton.addEventListener("click", clearAll);
+
+function clearAll(){
+    display.textContent = "";
+}
+
 //Create state boolean variables that do the following:
 //num1 should hold the first term
 //num2 should hole the second term
@@ -17,12 +23,17 @@ function displayFunction(num){
     if(display.textContent.length <9){
         if(clear == true){
             //Reset the display here. Use display.textContent to do so 
+            display.textContent = num;
             //set 'clear' to false
+            clear = false;
             //return 'clear'
+            return clear;
         }
         if(clear == false){
             //Append to the display. Use display.textContent to do so 
             //return 'clear'
+            display.textContent += num;
+            return clear;
         }
     }
 }
@@ -37,38 +48,52 @@ function operation(state, operand){
     if(num1 == ""){
         //This is the first part of your operation, this if statement is done when num1 is equal to blank
         //Use the converter function on num1
+        num1 = converter(display.textContent);
         //set clear to true
+        clear = true;
         //return the variable operand
+        return operand;
     }
     if(state !== "" && operand != "equal"){
         num2 = converter(display.textContent)
         if(state == "plus")
             //set the display to the value of the operation num1 + num2 using the converter function
+            display.textContent = (num1 + num2).toString();
         if(state == "minus")
             //set the display to the value of the operation num1 - num2 using the converter function
+            display.textContent = (num1 - num2).toString();
         if(state == "multi")
             //set the display to the value of the operation num1 * num2 using the converter function
+            display.textContent = (num1 * num2).toString();
         if(state == "divide"){
             //set the display to the value of the operation num1 / num2 using the converter function
+            display.textContent = (num1/num2).toString();
         }
         num1 = converter(display.textContent)
         //Reset the num2 variable to blank
+        num2 = "";
     }
     if(state != "" && operand == "equal"){
         num2 = converter(display.textContent)
         if(state == "plus")
             //set the display to the value of the operation num1 + num2 using the converter function
+            display.textContent = (num1 + num2).toString();
         if(state == "minus")
             //set the display to the value of the operation num1 - num2 using the converter function
+            display.textContent = (num1 - num2).toString();
         if(state == "multi")
             //set the display to the value of the operation num1 * num2 using the converter function
+            display.textContent = (num1 * num2).toString();
         if(state == "divide"){
             //set the display to the value of the operation num1 / num2 using the converter function
+            display.textContent = (num1 / num2).toString();
         }
         num1 = "", num2 =""
     }
     //set clear to true
+    clear = "";
     //return the value operand
+    return operand;
 }
 
 
@@ -77,14 +102,29 @@ for(let i=0; i<buttons.length; i++){
     buttons[i].addEventListener('click', function(e){
         if(this.id == "equal"){
             //Here you want to use the opeartion function when equal is pressed. Make sure you set the return value as the new state and the second parameter in the operation function to this.id
+            state = operation(state, this.id)
         }
         if(this.dataset.number == undefined && this.id !== "equal"){
             //When you look at your html elements they contain something called data-number. These are custom attributes that you can be used as values on your elements. The buttons that are associated with numbers have there corresponding numbers, but some values don't. This block of code is here when a button is clicked when it doesn't have a data-number attribute but also doesn't have an id of 'equal'. 
             //set state equal to this.id
+            state = this.id;
             //Use the operation function again. Remember to use state to receive the return function and use this.id as the second parameter 
+            state = operation(state, this.id)
         }
         if(this.dataset.number != undefined){
             //Use the displayFunction to update the display element. Use the variable clear to receive the return element and use this.dataset.number as the parameter in the function
+        clear = displayFunction(this.dataset.number)
         }
+
+
     })
+
+    document.querySelector('#clearButton').addEventListener('click', function(){
+        display.textContent = "";
+        num1 = "";
+        num2 = "";
+        state = "";
+        clear = false;
+    })
+
 }
